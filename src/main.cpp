07051c4254d7b1552dbb897492810f9df8a6c3e9
Artem Suvorov <suvorov.artem@hotmail.com>
2023-02-03 17:26:18 +0300

    draw a rectangle via an index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,46 @@
 #include "graphics/shader.h"
 
 class Pseudocraft : public Application
-{
+{    
 private:
     uint32_t shader;
 
 protected:
     virtual void OnInit()
     {
-        std::array<float, 6> postions = 
+        std::array<float, 8> postions = 
         {
             -0.5f, -0.5f,
-            0.0f,  0.5f,
-            0.5f, -0.5f
+             0.5f, -0.5f,
+             0.5f,  0.5f,
+
+            //  0.5f,  0.5f,
+            -0.5f,  0.5f,
+            // -0.5f, -0.5f,
         };
-        uint32_t size = 6 * sizeof(float);
+        uint32_t positionSize = 8 * sizeof(float);
+        
+        std::array<uint32_t, 6> indices =
+        {
+            0, 1, 2,
+            2, 3, 0
+        };
+        uint32_t indexSize = 6 * sizeof(uint32_t);
 
         uint32_t buffer;
         glGenBuffers(1, &buffer);
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, size, &postions, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), &postions, GL_STATIC_DRAW);
 
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        uint32_t indexBufferObject;
+        glGenBuffers(1, &indexBufferObject);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexSize, &indices, GL_STATIC_DRAW);
+
+        // glBindBuffer(GL_ARRAY_BUFFER, 0);
 
         ShaderSource source = ParseShader("res/shaders/blue.shader");
         shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -37,7 +53,8 @@ protected:
 
     virtual void OnRender()
     {
-        glDrawArrays(GL_TRIANGLES, 0, 3);   
+        // glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
     }
 
     virtual void OnDestroy()
